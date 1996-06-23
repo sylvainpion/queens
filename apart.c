37f81b5350f74fc9697799113a35ddffab24f869
Sylvain Pion <pion.sylvain@gmail.com>
1996-06-23 20:27:01 +0000

    Gestion des debordements.

--- apart.c
+++ apart.c
@@ -3,6 +3,11 @@
 
 #include "algo.h"
 
+#define MODULO 65536
+
+static unsigned int nb1 = 0;
+static unsigned int nb2 = 0;
+
 int main ()
 {
   unsigned int i = 1;
@@ -11,6 +16,10 @@ int main ()
     printf("1ere col: %d\n", i);
 #endif
     pipo_str (i, i<<1, i>>1);
+    nb2 += nb % MODULO;
+    nb1 += (nb / MODULO) + (nb2 / MODULO);
+    nb2 = nb2 % MODULO;
+    nb = 0;
   };
   if ((RG&1) == 1) {
     unsigned int j = 1;
@@ -22,8 +31,12 @@ int main ()
       printf("2eme col: %d\n", j);
 #endif
       pipo_str (i|j, ((i<<1)|j)<<1, ((i>>1)|j)>>1);
+      nb2 += nb % MODULO;
+      nb1 += (nb / MODULO) + (nb2 / MODULO);
+      nb2 = nb2 % MODULO;
+      nb = 0;
     };
   };
-  printf("%d reines => %d solutions\n", RG, 2*nb);
+  printf("%d reines => 2*(%d + %d * %d) solutions\n", RG, nb2, nb1, MODULO);
   return 0;
 }
